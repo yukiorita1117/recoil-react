a63c5d33cec355de5c69fed2e4f222daee7cf02a
折田幸夫 <3204704@gmail.com>
2020-05-17 18:24:32 +0900

    add TaskItem component

---
+++ src/components/TaskItem.tsx
@@ -0,0 +1,43 @@
+import React, { useState, FC } from "react";
+import { useRecoilValue, useSetRecoilState, useRecoilState } from "recoil";
+import { Task, taskState } from "../atoms/Task";
+
+interface TaskItemProps {
+  task: Task;
+  index: number;
+}
+
+const removeTasksAtIndex = (tasks: Task[], index: number) => {
+  return [...tasks.slice(0, index), ...tasks.slice(index + 1)];
+};
+
+const replaceTasksAtIndex = (tasks: Task[], index: number, newTask: Task) => {
+  return [...tasks.slice(0, index), newTask, ...tasks.slice(index + 1)];
+};
+
+const TaskItem: FC<TaskItemProps> = ({ task, index }) => {
+  const [tasks, setTasks] = useRecoilState(taskState);
+
+  const onChange = () => {
+    const newTasks = replaceTasksAtIndex(tasks, index, {
+      ...task,
+      completed: !task.completed,
+    });
+    setTasks(newTasks);
+  };
+
+  const onClick = () => {
+    const newTasks = removeTasksAtIndex(tasks, index);
+    setTasks(newTasks);
+  };
+
+  return (
+    <li key={index}>
+      <input type="checkbox" checked={task.completed} onChange={onChange} />
+      {task.title}
+      <button onClick={onClick}>削除</button>
+    </li>
+  );
+};
+
+export default TaskItem;
