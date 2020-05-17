00fe9475e558de93a3d24fd5bbed1be7279281fe
折田幸夫 <3204704@gmail.com>
2020-05-17 18:24:11 +0900

    TODO管理追加

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { useSetRecoilState } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import { taskState } from "../atoms/Task";
+import TaskItem from "./TaskItem";
 
 const TaskInput = () => {
   const [title, setTitle] = useState("");
@@ -8,26 +9,38 @@ const TaskInput = () => {
   // useStateと同じように使えるが、globalなstateの書き換えになる。
   // stateに書き込まない場合はuseRecoilValueが推奨されている。
   const setTasks = useSetRecoilState(taskState);
+  console.log("atom/Task のlog", taskState);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
   };
-  const onClick = () => {};
+  const handleClick = () => {
+    setTasks((t) => {
+      return [...t, { title, completed: false }];
+    });
+    setTitle("");
+  };
   return (
     <div>
       <label>
         TODOタスク
         <input type="text" value={title} onChange={onChange} />
       </label>
-      <button onClick={onClick}>登録</button>
+      <button onClick={handleClick}>登録</button>
     </div>
   );
 };
 
 export const TaskList = () => {
+  const tasks = useRecoilValue(taskState);
   return (
     <>
       <TaskInput />
+      <ul>
+        {tasks.map((t, index) => {
+          return <TaskItem task={t} index={index} key={index} />;
+        })}
+      </ul>
     </>
   );
 };
---
+++ src/components/Todo.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+
+const Todo = () => {
+  return <></>;
+};
+
+export default Todo;
