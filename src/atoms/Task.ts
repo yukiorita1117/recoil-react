e5c56b8174e8fbac7c453558d604ebefef4d6b66
折田幸夫 <3204704@gmail.com>
2020-05-17 17:52:11 +0900

    atom設定 / feature task component

--- src/atoms/Task.ts
+++ src/atoms/Task.ts
@@ -7,6 +7,8 @@ export interface Task {
 
 const initialTasks: Task[] = [];
 
+// Recoilでは、グローバルな（複数コンポーネントで共有される）ステートはAtom(データストア)と呼ぶ。
+// keyを設定しない場合、もしくは被ったらランタイムエラー。しかし許容範囲と見れる。
 export const taskState = atom({
   key: "task",
   default: initialTasks,
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSetRecoilState } from "recoil";
+import { taskState } from "../atoms/Task";
 
-const TaskList = () => {
-  return <div>タスクリスト</div>;
+const TaskInput = () => {
+  const [title, setTitle] = useState("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+  const onClick = () => {};
+  return (
+    <div>
+      <label>
+        TODOタスク
+        <input type="text" value={title} onChange={onChange} />
+      </label>
+      <button onClick={onClick}>登録</button>
+    </div>
+  );
+};
+
+export const TaskList = () => {
+  return (
+    <>
+      <TaskInput />
+    </>
+  );
 };
 
 export default TaskList;
